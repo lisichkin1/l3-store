56cf4a0fa6913920cf777d778332bff2f76b71df
ivan <ivanlisichkin@icloud.com>
2024-01-05 20:34:44 +0400

    feat:add user id to fetch

--- src/modules/productDetail/productDetail.ts
+++ src/modules/productDetail/productDetail.ts
@@ -4,6 +4,7 @@ import { formatPrice } from '../../utils/helpers';
 import { ProductData } from 'types';
 import html from './productDetail.tpl.html';
 import { cartService } from '../../services/cart.service';
+import { userService } from '../../services/user.service';
 
 class ProductDetail extends Component {
   more: ProductList;
@@ -19,8 +20,12 @@ class ProductDetail extends Component {
   async render() {
     const urlParams = new URLSearchParams(window.location.search);
     const productId = Number(urlParams.get('id'));
-
-    const productResp = await fetch(`/api/getProduct?id=${productId}`);
+    const userId = await userService.getUserId();
+    const productResp = await fetch(`/api/getProduct?id=${productId}`, {
+      headers: {
+        'x-userid': userId
+      }
+    });
     this.product = await productResp.json();
 
     if (!this.product) return;
@@ -44,10 +49,10 @@ class ProductDetail extends Component {
       });
 
     fetch('/api/getPopularProducts', {
-        headers: {
-          'x-userid': window.userId
-        }
-      })
+      headers: {
+        'x-userid': userId
+      }
+    })
       .then((res) => res.json())
       .then((products) => {
         this.more.update(products);
